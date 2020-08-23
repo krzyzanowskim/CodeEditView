d691cd247d7af7f79725780f64a3d28cf0efe606
Marcin Krzyzanowski <marcin.krzyzanowski@gmail.com>
2020-08-23 12:28:59 +0200

    Init Xcode sample project

---
+++ Xcode/CodeEditViewSample/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  CodeEditViewSample
+//
+//  Created by Marcin Krzyzanowski on 23/08/2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
