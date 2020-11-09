f16ebbdece7514eed08760ec68335ce730ce3f4a
Marcin Krzyzanowski <marcin.krzyzanowski@gmail.com>
2020-11-09 21:55:27 +0100

    Cleanup

--- Sources/CodeEditView/TextAttributes/TextAttributes.swift
+++ Sources/CodeEditView/TextAttributes/TextAttributes.swift
@@ -21,49 +21,6 @@ final class TextAttributes {
     }
 
     func adjustAttributedRanges(willRemoveRange removeRange: Range, in textStorage: TextStorage) {
-//        var didUpdateRange = false
-
-        // for-loop doesnt work here becase
-        // iterates over `ranges` that also mutates
-        // What we want is to iterated over mutated ranges (not the initial ranges)
-//        self.ranges = ranges.reduce(into: [:]) { (r, kv) in
-//            let range = kv.key
-//            let attributes = kv.value
-//            r[range] = attributes
-//
-//            guard range.overlaps(removeRange) else {
-//                return
-//            }
-//
-//            let clamped = range.clamped(to: removeRange)
-//            var lowerRange: Range?
-//            var upperRange: Range?
-//
-//            // Split and ajust, results in two new ranges if needed.
-//
-//            if range.start != clamped.start {
-//                lowerRange = Range(range.start..<clamped.start)
-//            }
-//
-//            if range.end != clamped.end {
-//                upperRange = Range(clamped.end..<range.end)
-//            }
-//
-//            if lowerRange != nil || upperRange != nil {
-//                // skip this range, aka remove
-//                // ranges.removeValue(forKey: range)
-//                r[range] = nil
-//
-//                if let rangeLower = lowerRange {
-//                    r[rangeLower] = attributes
-//                }
-//
-//                if let rangeUpper = upperRange {
-//                    r[rangeUpper] = attributes
-//                }
-//            }
-//        }
-
         for (range, attributes) in ranges where range.overlaps(removeRange) {
             let clamped = range.clamped(to: removeRange)
             var lowerRange: Range?
